cee6ed676c0b3693b16311243b2035c09ffe3870
Sai Pc <schand31@asu.edu>
2016-04-30 21:19:51 -0700

    updated emailing checks

--- email_gmail.php
+++ email_gmail.php
@@ -7,7 +7,8 @@
  */
 require('PD.php');
 /* connect to gmail */
-$hostname = '{imap.gmail.com:993/imap/ssl/novalidate-cert}EHI Received Emails';
+$hostname = '{imap.gmail.com:993/imap/ssl/novalidate-cert}EHI Emails';
+//$hostname = '{imap.gmail.com:993/imap/ssl/novalidate-cert}EHI Emails';
 
 /* try to connect */
 $inbox = imap_open($hostname,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
@@ -18,29 +19,35 @@ $emails = imap_search($inbox,'ALL');
 /* if emails are returned, cycle through each... */
 if($emails) {
     /* begin output var */
-    $output = '';
+    echo "Emails found<br>";
+
 
     /* put the newest emails on top */
-    rsort($emails);
+//    rsort($emails);
 
     /* for every email... */
     foreach($emails as $email_number) {
+        $output = '';
         /* get information specific to this email */
         $overview = imap_fetch_overview($inbox,$email_number,0);
-        $message = imap_fetchbody($inbox,$email_number,2);
+//        $message = imap_fetchbody($inbox,$email_number,2);
 
         /* output the email header information */
-        $output.= '<div class="toggler '.($overview[0]->seen ? 'read' : 'unread').'">';
-        $output.= '<span class="subject">'.$overview[0]->subject.'</span> ';
-        $output.= '<span class="from">'.$overview[0]->from.'</span>';
-        $output.= '<span class="date">on '.$overview[0]->date.'</span>';
-        $output.= '</div>';
+//        $output.= '<div class="toggler '.($overview[0]->seen ? 'read' : 'unread').'">';
+        $output.= $overview[0]->subject;
+//        $output.= '<span class="from">'.$overview[0]->from.'</span>';
+//        $output.= '<span class="date">on '.$overview[0]->date.'</span>';
+//        $output.= '</div>';
 
         /* output the email body */
-        $output.= '<div class="body">'.$message.'</div>';
+//        $output.= '<div class="body">'.$message.'</div>';
+        $output.= "<br>\n";
+        echo $output;
     }
 
-    echo $output;
+
+} else {
+    echo 'No emails';
 }
 
 /* close the connection */
