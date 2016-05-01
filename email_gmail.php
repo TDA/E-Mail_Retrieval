f12bed0497bffc041590a1433ed96df41dc53dfe
Sai Pc <schand31@asu.edu>
2016-04-30 23:46:32 -0700

    retrieving matches

--- email_gmail.php
+++ email_gmail.php
@@ -15,37 +15,31 @@ $inbox = imap_open($hostname,$username,$password) or die('Cannot connect to Gmai
 
 /* grab emails that failed */
 $emails = imap_search($inbox,'FROM "mailer-daemon@googlemail.com"');
-
+$hashmap = Array();
 /* if emails are returned, cycle through each... */
 if($emails) {
     /* begin output var */
     echo "Emails found<br>\n";
 
-
-    /* put the newest emails on top */
-//    rsort($emails);
-
     /* for every email... */
     foreach($emails as $email_number) {
         $output = '';
         /* get information specific to this email */
-        $overview = imap_fetch_overview($inbox,$email_number,0);
-        $message = imap_fetchbody($inbox,$email_number,2);
 
-        //$re = "/.*Google tried to deliver your message, but it was rejected by the server for the recipient domain (.*) by (.*)\\. \\[(.*)\\].*/";
-        //preg_match($re, $message, $matches);
+        $message = imap_fetchbody($inbox, $email_number, 1.2);
+        if(!strlen($message)>0){
+            $message = imap_fetchbody($inbox, $email_number, 1);
+        }
 
-        /* output the email header information */
-        $output.= $overview[0]->subject;
-        $output.= $overview[0]->date;
-//        $output.= implode(",", $matches);
-        $output.= $message;
-//        $output.= '</div>';
-
-        /* output the email body */
-//        $output.= '<div class="body">'.$message.'</div>';
-        $output.= "<br>\n";
-        echo $output;
+        $re = "/.*Google tried to deliver your message, but it was rejected by the server for the recipient domain (.*) by (.*)\\. \\[(.*)\\].*/";
+        preg_match($re, $message, $matches);
+        if (!empty($matches)) {
+            echo $matches[1] . " --- " . $matches[2] . " --- " . $matches[3] . "\n";
+            $hashmap[$matches[1]] = [$matches[2], $matches[3]];
+        }
+//        $output = implode(",", $matches);
+//        $output.= "<br>\n";
+//        echo $output;
     }
 
 
@@ -53,6 +47,7 @@ if($emails) {
     echo 'No emails';
 }
 
+echo implode("  ", $hashmap);
 /* close the connection */
 imap_close($inbox);
 ?>
