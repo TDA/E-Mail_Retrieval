96851e3860f21731738eb7afca6cb2dc1634f137
Sai Pc <schand31@asu.edu>
2016-05-01 02:24:47 -0700

    making the function reusable

--- email_gmail.php
+++ email_gmail.php
@@ -6,48 +6,60 @@
  * Time: 7:45 PM
  */
 require('PD.php');
-/* connect to gmail */
-$hostname = '{imap.gmail.com:993/imap/ssl/novalidate-cert}EHI Emails';
-//$hostname = '{imap.gmail.com:993/imap/ssl/novalidate-cert}EHI Emails';
+function getEMail($username, $password, $condition)
+{
+    /* connect to gmail */
+    $hostname = '{imap.gmail.com:993/imap/ssl/novalidate-cert}EHI Emails';
 
-/* try to connect */
-$inbox = imap_open($hostname,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
+    /* try to connect */
+    $inbox = imap_open($hostname, $username, $password) or die('Cannot connect to Gmail: ' . imap_last_error());
 
-/* grab emails that failed */
-$emails = imap_search($inbox,'FROM "mailer-daemon@googlemail.com"');
-$hashmap = Array();
-/* if emails are returned, cycle through each... */
-if($emails) {
-    /* begin output var */
-    echo "Emails found<br>\n";
+    /* grab emails that satisfy condition */
+    $emails = imap_search($inbox, $condition);
+    $messages = Array();
+    /* if emails are returned, cycle through each... */
+    if ($emails) {
+        /* begin output var */
+        echo "Emails found<br>\n";
 
-    /* for every email... */
-    foreach($emails as $email_number) {
-        $output = '';
-        /* get information specific to this email */
+        /* for every email... */
+        foreach ($emails as $email_number) {
+            $output = '';
+            /* get information specific to this email */
 
-        $message = imap_fetchbody($inbox, $email_number, 1.2);
-        if(!strlen($message)>0){
-            $message = imap_fetchbody($inbox, $email_number, 1);
+            $message = imap_fetchbody($inbox, $email_number, 1.2);
+            if (!strlen($message) > 0) {
+                $message = imap_fetchbody($inbox, $email_number, 1);
+            }
+//            echo $message."\n";
+            $messages[$email_number] = $message;
         }
 
-        $re = "/.*Google tried to deliver your message, but it was rejected by the server for the recipient domain (.*) by (.*)\\. \\[(.*)\\].*/";
-        preg_match($re, $message, $matches);
-        if (!empty($matches)) {
-            echo $matches[1] . " --- " . $matches[2] . " --- " . $matches[3] . "\n";
-            $hashmap[$matches[1]] = [$matches[2], $matches[3]];
-        }
-//        $output = implode(",", $matches);
-//        $output.= "<br>\n";
-//        echo $output;
+    } else {
+        echo 'No emails';
+        return null;
     }
-
-
-} else {
-    echo 'No emails';
+    /* close the connection */
+    imap_close($inbox);
+    return $messages;
 }
 
-print_r($hashmap);
-/* close the connection */
-imap_close($inbox);
+
+//$messages = getEMail($username, $password, 'FROM "mailer-daemon@googlemail.com"');
+////print_r($messages);
+//
+//$re = "/.*Google tried to deliver your message, but it was rejected by the server for the recipient domain (.*) by (.*)\\. \\[(.*)\\].*/";
+//
+//$hashmap = Array();
+//foreach($messages as $message) {
+//    preg_match($re, $message, $matches);
+//    if (!empty($matches)) {
+//        echo $matches[1] . " --- " . $matches[2] . " --- " . $matches[3] . "\n";
+//        $hashmap[$matches[1]] = [$matches[2], $matches[3]];
+//    }
+//}
+//print_r($hashmap);
+
+$messages = getEMail($username, $password, 'TO '.$argv[1]);
+print_r($messages[0]);
 ?>
